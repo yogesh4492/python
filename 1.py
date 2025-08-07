898b7f54a37206017e1cfb75c26e4891ad96be1a
yogesh4492 <you@example.com>
2025-08-07 13:37:19 +0530

    modified:   1.py 	new file:   assignment/1.py 	new file:   assignment/2.py 	modified:   practice.ipynb 	modified:   topic1.ipynb

--- 1.py
+++ 1.py
@@ -1,7 +1,39 @@
-print("hello world");
 
-print("hello",end=" ")
-print(" welcome to python")
+# roll=input("enter your roll no= ")
+# std=input("enter your standard= ")
+# percenatage=input("enter yoyur percenatge= ")
+# grade=input("enter your grade= ")
+# fees=input("enter your total fees= ")
 
-roll=input("enter your roll no= ")
-print(roll)
+
+# print("Roll No    = ",roll)
+# print("Standard   = ",std)
+# print("Percentage = ",percenatage)
+# print("Grade      = ",grade)
+# print("Fees       = ",fees)
+
+num1,num2=input("Enter Num1 and num2= ").split()
+
+print("num1= ",num1,"num2= ",num2)
+
+num1=int(num1)
+num2=int(num2)
+print("addition of",num1,"and",num2,"= ",num1+num2)
+
+num1=int(input("Enter Num1= "))
+num2=int(input("Enter Num2= "))
+print("Subtraction of",num1,"and",num2,"= ",num1-num2)
+
+num1=int(input("Enter Num1= "))
+num2=int(input("Enter Num2= "))
+print("Multiplication of",num1,"and",num2,"= ",num1*num2)
+
+num1=int(input("Enter Num1= "))
+num2=int(input("Enter Num2= "))
+res= (float(num1)/float(num2))
+print("Division of",num1,"and",num2,"= ",num1/num2)
+
+num1=int(input("Enter Num1= "))
+num2=int(input("Enter Num2= "))
+print("Modulus of",num1,"and",num2,"= ",num1%num2)
+
---
+++ assignment/1.py
@@ -0,0 +1,4 @@
+# Que-1
+print("Hello World !!")
+# Que-2
+print("Good Morning, My Name is Yogesh Patel")
