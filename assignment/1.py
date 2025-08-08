cec8d9f6b2030ea38b7e9ddc1eea211d20e367dc
yogesh4492 <you@example.com>
2025-08-08 18:17:07 +0530

    modified:   assignment/1.y 	modified:   assignment/2.py 	new file:   assignment/Question 1.ipynb 	new file:   assignment/python_assignment_1731399484.pdfp

--- assignment/1.py
+++ assignment/1.py
@@ -1,4 +1,7 @@
-# Que-1
+# Que-1-Introduction to Python
+# i):- Write a Python program that prints "Hello, World!!
 print("Hello World !!")
-# Que-2
+# ii):- Set up Python on your local machine and write a program to display your name
 print("Good Morning, My Name is Yogesh Patel")
+
+# Que-2 :- 
--- assignment/2.py
+++ assignment/2.py
@@ -1,9 +1,7 @@
-
+row=int(input("Enter The Number "))
 i=1
 j=1
-row=int(input("Enter number of rows: "))
 for i in range(1,row+1):
     for j in range(1,i+1):
-        print("*", end=" ")
-    print()  # Move to the next line after each row
- 
+        print("*",end=" ")
+    print()
