8e83c66a4459e57b026b4f89918256c9df61a9e4
yogesh4492 <you@example.com>
2025-08-12 20:23:42 +0530

    new file:   practice/29.c 	new file:   practice/29.cpp 	new file:   practice/29.exe 	modified:   practice/index.ipynb

---
+++ practice/29.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int row,i,j;
+    printf("Enter the row= ");
+    scanf("%d",&row);
+    for(i=1;i<=row;i++){
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
