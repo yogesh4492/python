f4700c2ab212ba80fe081cfe29f11607dc604cc8
yogesh4492 <you@example.com>
2025-08-12 16:10:09 +0530

    modified:   Anjali_mam_study/lecture 3&4 d12&14/1.py 	modified:   Anjali_mam_study/lecture 3&4 d12&14/first.ipynb 	new file:   Anjali_mam_study/lecture 3&4 d12&14/task.py 	new file:   Anjali_mam_study/lecture 3&4 d12&14/task2.py

---
+++ Anjali_mam_study/lecture 3&4 d12&14/task.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter number1= "))
+num2=int(input("enter number2= "))
+
+if num1>num2:
+    print("num1 iss greter")
+else:
+    print("num2 is greter")
---
+++ Anjali_mam_study/lecture 3&4 d12&14/task2.py
@@ -0,0 +1,5 @@
+num=int(input("enter number"))
+if num>0:
+    print("Number is positive")
+else :
+    print("number negative")
