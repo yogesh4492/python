9e5a6a6622826e5a7c98fd721409813c6a415f5b
yogesh4492 <you@example.com>
2025-08-17 10:15:23 +0530

    new file:   practice/36.cpp 	new file:   practice/36.exe 	modified:   practice/index.ipynb 	modified:   practice/index2.ipynb

---
+++ practice/36.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num1,num2;
+    printf("Ente Number 1= ");
+    scanf("%d",&num1);
+    printf("Ente Number 2= ");
+    scanf("%d",&num2);
+    printf("result of %d  ",num1 & num2);
+    return 0;
+}
