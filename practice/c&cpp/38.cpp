8cc3513a1e525845aec5a51ac3a5117b8aceb55f
yogesh4492 <you@example.com>
2025-08-21 14:45:29 +0530

    new file:   practice/c&cpp/38.cpp 	modified:   practice/c&cpp/38.exe

---
+++ practice/c&cpp/38.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int row,i,k,j;
+    printf("\n Enterw Row= ");
+    scanf("%d",&row);
+    int spc=row-1;
+    for(i=1;i<=row;i++){
+        for(k=1;k<=spc;k++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+        spc--;
+    }
+    spc=0;
+    for(i=1;i<=row;i++){
+        for(k=spc;k>=1;k--){
+            printf(" ");
+        }
+        for(j=5;j>=i;j--){
+            printf("* ");
+        }
+        printf("\n");
+        spc++;
+    }
+
+    return 0;
+
+}
