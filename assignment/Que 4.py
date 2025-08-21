b9638741d11fc4bdabb7cb89f3c701650c5f53b9
yogesh4492 <you@example.com>
2025-08-21 14:08:46 +0530

    modified:   assignment/Que 4.py

--- assignment/Que 4.py
+++ assignment/Que 4.py
@@ -61,3 +61,14 @@ else:
     print("You are Not Eligible for Blood Donation Because Your age is less than 18 year")
 
 #-----------------------------------------------------------------------------------------
+
+"""
+Practical Example 1: Write a Python program to print each fruit in a list using a simple for 
+loop. List1 = ['apple', 'banana', 'mango']
+
+
+
+"""
+List1 = ['apple', 'banana', 'mango']
+for i in List1:
+    print(i)
