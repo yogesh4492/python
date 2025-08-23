de4db06f8bb2d1331327fb4a72300b4465697164
yogesh4492 <you@example.com>
2025-08-23 22:15:13 +0530

    modified:   Anjali_mam_study/lecture 7&8/2.py

--- Anjali_mam_study/lecture 7&8/2.py
+++ Anjali_mam_study/lecture 7&8/2.py
@@ -7,14 +7,15 @@ while True:
     print("4. Level 4(1-100) 5 Chance Only")
     print("5. Level 5(1-50) 5 chance only")
     print("6. Level 6(1-50) 3 chance only")
-    # print("if you won all level then you called a genius")
+    print("7. Exit ")
     choice=int(input("ENter Your choice= "))
     computer=random.randint(1,100)
+    count=0
     if choice==1:
         print("-------Welcome to level  1------------")
         while True:
             user=int(input("Enter Number= "))
-            if user>100 or user<0:
+            if user>100 or user<=0:
                 print("Invalid range guess please guess In your range For Winning")
             else:
                 if user>computer:
@@ -29,10 +30,10 @@ while True:
         print("-----Welcome to level 2---------")
         limit=int(input("Enter Number of Chance= "))
         print()
-        for i in range(0,limit):
-            print(f"Current Chance= {i+1}/{limit}")
+        for i in range(1,limit+1):
+            print(f"Current Chance= {i}/{limit}")
             user=int(input("Enter Number= "))
-            if user>100 or user<0:
+            if user>100 or user<=0:
                 print("Invalid range please guess in your range to win")
             else:
                 if user>computer:
@@ -44,8 +45,81 @@ while True:
                     print("--------Level 2 Completed-----------")
                     print()
                     break
-            
-    
+    elif choice==3:
+        print("-------Welcome to level 3---------")
+        limit=10
+        print()
+        for i in range(1,limit+1):
+            print(f"Current Chance= {i}/{limit}")
+            user=int(input("Enter Number= "))
+            if user>100 or user<=0:
+                print("Invalid range please guess in your range to win")
+            else:
+                if user>computer:
+                    print("Hint : To High")
+                elif user<computer:
+                    print("Hint: To Low")
+                else:
+                    print("You win")
+                    print("--------Level 3 Completed-----------")
+                    print()
+                    break
+    elif choice==4:
+        print("-------Welcome to level 4-------")
+        limit=5
+        print()
+        for i in range(1,limit+1):
+            print(f"Current Chance= {i}/{limit}")
+            user=int(input("Enter Number= "))
+            if user>100 or user<=0:
+                print("Invalid range please guess in your range to win")
+            else:
+                if user>computer:
+                    print("Hint : To High")
+                elif user<computer:
+                    print("Hint: To Low")
+                else:
+                    print("You win")
+                    print("--------Level 3 Completed-----------")
+                    print()
+                    break
+    elif choice==5:
+        print("-----Welcome to level 5------")
+        computer=random.randint(1,50)
+        limit=5
+        for i in range(1,limit+1):
+            user=int(input("Enter number= "))
+            if user>50 or user<=0:
+                print("invalid Range please guess in range to win")
+            else:
+                if user>computer:
+                    print("Hint: To High")
+                elif user<computer:
+                    print("Hint: To Low")
+                else:
+                    print("You Win!!!")
+                    print("-------------level 5 completed-----")
+                    break
+    elif choice==6:
+        print("-----Welcome to level 6---------")
+        computer=random.randint(1,50)
+        limit=3
+        for i in range(1,limit+1):
+            user=int(input("Enter Number= "))
+            if user>50 or user<=0:
+                print("invalid range input")
+            else:
+                if user>computer:
+                    print("Hint: To High ")
+                elif user<computer:
+                    print("Hint: To Low")
+                else:
+                    print("You win !!!")
+                    print("Level 6 completed")
+                    break   
+    elif choice==7:
+        print("--------Thank You For Playing------------")
+        break         
     else:
         print("Invalid input")
 
